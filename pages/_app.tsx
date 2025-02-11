4a4164e6cf7460ad3e3f852dc73aa63d6e81c447
HansImmanuel <154997208+HansImmanuel@users.noreply.github.com>
2025-02-11 10:34:06 +0700

    haha

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "white" }}>
       <h1>Hans</h1>
+      <Component {...pageProps} />
     </div>
   );
 }
