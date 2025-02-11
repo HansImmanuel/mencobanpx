cb156846edd29caddb0cb5e6037f50af9716d060
HansImmanuel <154997208+HansImmanuel@users.noreply.github.com>
2025-02-11 10:27:57 +0700

    woi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "white" }}>
+      <h1>Hans</h1>
+    </div>
+  );
 }
